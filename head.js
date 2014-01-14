e6a5d297057c3e8f3037589ebb6c0984ec695b8a
hoppin <johnhoppin@yahoo.com>
2014-01-14 10:50:12 -0800

    Update head.js

--- head.js
+++ head.js
@@ -1,6 +1,10 @@
 var hogs = prompt('How many hogs?','Hogs')
 var side = prompt*2;
-  console.log("There are " + prompt + " sides of pork.")
+
+// primal=["subprimal";"subprimal"]
+// some subprimals eliminate other subprimals within the side
+// ex: boston butt is cut from the top of the shoulder and it produces a picnic ham, a fore shank and trotter.
+// it eliminates a whole shoulder, a long shoulder (which is the whole shoulder w shank), and a long shoulder with hoof.
 
 head=["cheek";"jowl";"split head"]
 shoulder=["boston butt";"picnic ham";"fore shank";"fore trotter";"whole shoulder";"long shoulder";"long shoulder w/hoof"]
@@ -8,3 +12,10 @@ middle=["porterhouse chops";"loin chops";"canadian loin";"ten rib rack";"tenderl
 ham=["ham";"rear shank";"rear trotter";"whole ham"]
 
 
+// an order format
+order=[orderNumber;accountNumber;orderedBy;orderedWho;dateOrdered;dateDelivered;cut;qty;price]
+
+// why do this? if you sell 200 hogs a week and you have to sell every bit because you have no cold storage or market
+// for frozen product.  you call your accounts all day and you take every order.  you fill your oldest accounts and then short
+// or sub out (picnic hams for boston butts, for example).  a web based tool thaat allowed you to count down and know what's
+// available
