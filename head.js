cfaf3e1b6dc0fd4de9aa0f69b3c1af28bee7c61a
hoppin <johnhoppin@yahoo.com>
2014-01-14 09:39:43 -0800

    Update head.js

--- head.js
+++ head.js
@@ -1,4 +1,9 @@
-var head (prompt);
-var side (prompt*2);
+var head = prompt('How many head?','Head')
+var side = prompt*2);
+
+head=["cheek";"jowl";"split head"]
+shoulder=["boston butt";"picnic ham";"fore shank";"fore trotter";"whole shoulder";"long shoulder";"long shoulder w/hoof"]
+middle=["porterhouse chops";"loin chops";"canadian loin";"ten rib rack";"tenderloin";"belly w/ribs";"spare ribs";"st. louis ribs";"belly";"whole middle"]
+ham=["ham";"rear shank";"rear trotter";"whole ham"]
 
 
