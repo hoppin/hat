6d9a3884755b3475c640112f73299d9105a47762
hoppin <johnhoppin@yahoo.com>
2014-01-15 15:53:27 -0800

    Update head.js

--- head.js
+++ head.js
@@ -1,8 +1,4 @@
-var hogs = prompt("How many hogs?",'Hogs');
-if(isNaN('hogs')>0) {
-  console.log("That is not a number.")
-  hogs;
-}
+var hogs = prompt("How many hogs?");
 var side = hogs*2;
 
 // primal=["subprimal";"subprimal"]
@@ -10,14 +6,37 @@ var side = hogs*2;
 // ex: boston butt is cut from the top of the shoulder and it produces a picnic ham, a fore shank and trotter.
 // it eliminates a whole shoulder, a long shoulder (which is the whole shoulder w shank), and a long shoulder with hoof.
 
+var sideCuts = [[head], [shoulder], [middle], [ham]
+
 head=["cheek";"jowl";"split head"]
 shoulder=["boston butt";"picnic ham";"fore shank";"fore trotter";"whole shoulder";"long shoulder";"long shoulder w/hoof"]
 middle=["porterhouse chops";"loin chops";"canadian loin";"ten rib rack";"tenderloin";"belly w/ribs";"spare ribs";"st. louis ribs";"belly";"whole middle"]
 ham=["ham";"rear shank";"rear trotter";"whole ham"]
 
+orderNumber=(previousOrder+1)
+prompt(accountNumber)="Account Number"
+prompt(accountPurchaser
+orderedBy:
+  orderedWho:
+  dateOrdered:
+  dateDelivered:
+  sideCut:
+  qty:
+  price
+}
 
 // an order format
-order=[orderNumber;accountNumber;orderedBy;orderedWho;dateOrdered;dateDelivered;cut;qty;price]
+var order= {
+  orderNumber:
+  accountNumber:
+  orderedBy:
+  orderedWho:
+  dateOrdered:
+  dateDelivered:
+  sideCut:
+  qty:
+  price
+}
 
 // why do this? if you sell 200 hogs a week and you have to sell every bit because you have no cold storage or market
 // for frozen product.  you call your accounts all day and you take every order.  you fill your oldest accounts and then short
